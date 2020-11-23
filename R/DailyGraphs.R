3442357d366ef73a8b4045a3447c9c131818fc9b
computergeek1100 <66804731+computergeek1100@users.noreply.github.com>
2020-11-23 15:28:18 -0500

    Updated all graphs and added test graph

--- R/DailyGraphs.R
+++ R/DailyGraphs.R
@@ -8,6 +8,8 @@ library(tigris)
 library(leaflet)
 library(googlesheets4)
 
+ICUcolors <- c("ICU" = "red", "Ventilator" = "blue")
+
 stateData <- read_sheet("https://docs.google.com/spreadsheets/d/1c2QrNMz8pIbYEKzMJL7Uh2dtThOJa2j1sSMwiDo5Gz4/edit#gid=1592746937", sheet = "Trends")
 
 stateData <- stateData%>%
@@ -18,20 +20,24 @@ stateData <- stateData%>%
 caseGraph <- ggplot(stateData, aes(date,cases))+geom_col()+geom_line(aes(y=rollmean(cases, 7,na.pad=TRUE)),color="blue")
 caseGraph <- plotly::ggplotly(caseGraph)
 
+testGraph <- ggplot(stateData, aes(date,tests))+geom_col()+geom_line(aes(y=rollmean(tests, 7,na.pad=TRUE)),color="blue")
+testGraph <- plotly::ggplotly(testGraph)
+
 posGraph <- ggplot(stateData,aes(date,percentPos))+geom_col()+geom_line(aes(y=rollmean(percentPos,7, na.pad=TRUE)),color="blue")
 posGraph <- ggplotly(posGraph)
 
 hospGraph <- ggplot(stateData,aes(date,currentHosp))+geom_col()+geom_line(aes(y=rollmean(currentHosp,7,na.pad=TRUE)),color='blue')
 hospGraph <- ggplotly(hospGraph)
 
-ICUGraph <- ggplot(stateData,aes(date,))+geom_col(aes(,ICU),color="red")+geom_line(aes(y=rollmean(ICU,7,na.pad=TRUE)),color='red')+
-  geom_col(aes(,vent),color='blue')+geom_line(aes(y=rollmean(vent,7,na.pad=TRUE)),color='blue')
+ICUGraph <- ggplot(stateData,aes(date,))+geom_col(aes(,ICU,fill="ICU"))+geom_line(aes(y=rollmean(ICU,7,na.pad=TRUE)),color='red')+
+  geom_col(aes(,vent,fill='Ventilator'))+geom_line(aes(y=rollmean(vent,7,na.pad=TRUE)),color='blue')+scale_color_manual(values = ICUcolors)
 ICUGraph <- ggplotly(ICUGraph)
 
 dailyDeathGraph <- ggplot(stateData,aes(date,dailyDeaths))+geom_col()+geom_line(aes(y=rollmean(dailyDeaths,7,na.pad=TRUE)),color='blue')
 dailyDeathGraph <- ggplotly(dailyDeathGraph)
 
 htmlwidgets::saveWidget(caseGraph, file="../graphs/DAILY_cases.html",selfcontained=FALSE,libdir="../graphs/plotlyJS",title='dailycases')
+htmlwidgets::saveWidget(testGraph, file="../graphs/DAILY_tests.html",selfcontained=FALSE,libdir="../graphs/plotlyJS",title='dailytests')
 htmlwidgets::saveWidget(posGraph,file="../graphs/DAILY_pos.html",selfcontained=FALSE,libdir="../graphs/plotlyJS",title='dailypos')
 htmlwidgets::saveWidget(hospGraph,file="../graphs/DAILY_hosp.html",selfcontained=FALSE,libdir="../graphs/plotlyJS",title='dailyhosp')
 htmlwidgets::saveWidget(ICUGraph,file="../graphs/DAILY_ICU.html",selfcontained=FALSE,libdir="../graphs/plotlyJS",title='dailyicu')
