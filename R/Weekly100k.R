db4cdbebe2c81dd7956eab2ba90d1c772b1be3d4
computergeek1100 <66804731+computergeek1100@users.noreply.github.com>
2020-11-30 16:48:17 -0500

    Tooltip change

--- R/Weekly100k.R
+++ R/Weekly100k.R
@@ -30,8 +30,8 @@ if(nrow(rateDataCleaned) < 44){
 rateDataCleaned$weekEnding <- sub("/","-",rateDataCleaned$weekEnding)
 rateDataCleaned$weekEnding <- as.Date(rateDataCleaned$weekEnding)
 
-cases100kGraph <- ggplot(rateDataCleaned,aes(weekEnding,casesPer100k, text=paste("Date: ", weekEnding,
-                                                                                 "Weekly Cases per 100k: ", casesPer100k)))+
+cases100kGraph <- ggplot(rateDataCleaned,aes(weekEnding,casesPer100k, text=paste("Week Ending: ", weekEnding,
+                                                                                 "Cases per 100k: ", casesPer100k)))+
   geom_line(color="blue")+
   labs(x="Week Ending", y = "Weekly Cases per 100,000 Residents")
 cases100kGraph <- ggplotly(cases100kGraph,tooltip="text",dynamicTicks=TRUE, originalData=FALSE)%>%config(displayModeBar=FALSE)
