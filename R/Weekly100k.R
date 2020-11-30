7413f9fe578fbb610dff7fed28b42667d4bdb18e
computergeek1100 <66804731+computergeek1100@users.noreply.github.com>
2020-11-30 16:47:35 -0500

    Weekly graph - new tooltips

--- R/Weekly100k.R
+++ R/Weekly100k.R
@@ -5,12 +5,12 @@ library(lubridate)
 library(htmlwidgets)
 library(googlesheets4)
 
-rateDataTMP <- read_sheet("https://docs.google.com/spreadsheets/d/1c2QrNMz8pIbYEKzMJL7Uh2dtThOJa2j1sSMwiDo5Gz4/edit#gid=1592746937", sheet = "Rate Trends by Age")
+rateData <- read_sheet("https://docs.google.com/spreadsheets/d/1c2QrNMz8pIbYEKzMJL7Uh2dtThOJa2j1sSMwiDo5Gz4/edit#gid=1592746937", sheet = "Rate Trends by Age")
 
-if(identical(rateDataCur,rateDataTMP)){
+if(identical(rateDataCur,rateData)){
   stop("Graphs already up to date")
 }else {
-rateDataCur <- rateDataTMP
+rateDataCur <- rateData
 rateDataCleaned <- rateDataCur%>%
   select(weekEnding=1,casesPer100k=14)%>%
   filter(row_number() >= 8)
@@ -30,9 +30,10 @@ if(nrow(rateDataCleaned) < 44){
 rateDataCleaned$weekEnding <- sub("/","-",rateDataCleaned$weekEnding)
 rateDataCleaned$weekEnding <- as.Date(rateDataCleaned$weekEnding)
 
-cases100kGraph <- ggplot(rateDataCleaned,aes(weekEnding,casesPer100k))+
+cases100kGraph <- ggplot(rateDataCleaned,aes(weekEnding,casesPer100k, text=paste("Date: ", weekEnding,
+                                                                                 "Weekly Cases per 100k: ", casesPer100k)))+
   geom_line(color="blue")+
-  labs(x="Week Ending", y = "Cases per 100,000 Residents")
-cases100kGraph <- ggplotly(cases100kGraph,dynamicTicks=TRUE, originalData=FALSE)%>%config(displayModeBar=FALSE)
+  labs(x="Week Ending", y = "Weekly Cases per 100,000 Residents")
+cases100kGraph <- ggplotly(cases100kGraph,tooltip="text",dynamicTicks=TRUE, originalData=FALSE)%>%config(displayModeBar=FALSE)
 htmlwidgets::saveWidget(cases100kGraph, file="../graphs/WEEKLY_100k.html",selfcontained=FALSE,libdir="../graphs/plotlyJS",title="weekly100k")
 }
