eefa2deee45930a5dad6b2e22c4570306d49b9fd
computergeek1100 <66804731+computergeek1100@users.noreply.github.com>
2020-12-04 10:39:23 -0500

    Added date labels to weekly graph

--- R/Weekly100k.R
+++ R/Weekly100k.R
@@ -17,6 +17,7 @@ rateDataCleaned <- rateDataCur%>%
 rateDataCleaned$casesPer100k <- as.numeric(as.character(rateDataCleaned$casesPer100k))
 rateDataCleaned$weekEnding <- sub("\\d{1,2}/\\d{1,2}-","", rateDataCleaned$weekEnding)
 rateDataCleaned$weekEnding <- sub(" \\(.*","",rateDataCleaned$weekEnding)
+
 if(nrow(rateDataCleaned) < 44){
   rateDataCleaned$weekEnding <- paste("2020",rateDataCleaned$weekEnding,sep="-")
 }else{
@@ -29,11 +30,13 @@ if(nrow(rateDataCleaned) < 44){
 }
 rateDataCleaned$weekEnding <- sub("/","-",rateDataCleaned$weekEnding)
 rateDataCleaned$weekEnding <- as.Date(rateDataCleaned$weekEnding)
+weeklyUpdated <- tail(rateDataCleaned$weekEnding, 1)
+weeklyUpdated <- format(weeklyUpdated, "%B %d, %Y")
 
 cases100kGraph <- ggplot(rateDataCleaned,aes(weekEnding,casesPer100k, group=1, text=paste("Week Ending: ", weekEnding,
                                                                                  "<br>Cases per 100k: ", casesPer100k)))+
   geom_line(color="blue")+
-  labs(x="Week Ending", y = "Weekly Cases per 100,000 Residents")
+  labs(title=paste("Latest data: Week ending", weeklyUpdated), x="Week Ending", y = "Weekly Cases per 100,000 Residents")
 cases100kGraph <- ggplotly(cases100kGraph,tooltip="text",dynamicTicks=TRUE, originalData=FALSE)%>%config(displayModeBar=FALSE)
 htmlwidgets::saveWidget(cases100kGraph, file="../graphs/WEEKLY_100k.html",selfcontained=FALSE,libdir="../graphs/plotlyJS",title="weekly100k")
 }
