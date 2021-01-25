92bbd84f32bc5a52325335f41bff3666c577debc
computergeek1100 <66804731+computergeek1100@users.noreply.github.com>
2021-01-24 20:00:00 -0500

    Update scrape.py

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -9,8 +9,6 @@ DRIVER_PATH = "/usr/bin/geckodriver"
 if platform == "darwin":
     DRIVER_PATH = "/usr/local/bin/geckodriver"
 
-XPATH_PREFIX = "/html/body/app-bootstrap/ng2-bootstrap/bootstrap/div/div/div/div/div/div[1]/div[2]/div/div[1]/div/div[1]/div/lego-report/lego-canvas-container/div/file-drop-zone/span/content-section/"
-
 options = Options()
 options.headless = True
 
@@ -21,11 +19,11 @@ driver.implicitly_wait(20)
 driver.get(
     "https://datastudio.google.com/u/0/reporting/f95ea2dd-e77a-45dc-9735-f60e8581ff32/page/o9ivB")
 date = driver.find_element_by_xpath(
-    XPATH_PREFIX + "div[4]/canvas-component/div/div/div[1]/div/div/lego-table/div/div[3]/div/div[2]").text
+    "/html/body/app-bootstrap/ng2-bootstrap/bootstrap/div/div/div/div/div/div[1]/div[2]/div/div[1]/div/div[1]/div/lego-report/lego-canvas-container/div/file-drop-zone/span/content-section/div[4]/canvas-component/div/div/div[1]/div/div/lego-table/div/div[3]/div/div[2]").text
 dose1 = driver.find_element_by_xpath(
-    XPATH_PREFIX + "div[3]/canvas-component/div/div/div[1]/div/div/lego-table/div/div[3]/div[1]/div[2]").text
+    "/html/body/app-bootstrap/ng2-bootstrap/bootstrap/div/div/div/div/div/div[1]/div[2]/div/div[1]/div/div[1]/div/lego-report/lego-canvas-container/div/file-drop-zone/span/content-section/div[3]/canvas-component/div/div/div[1]/div/div/lego-table/div/div[3]/div[1]/div[2]").text
 dose2 = driver.find_element_by_xpath(
-    XPATH_PREFIX + "div[3]/canvas-component/div/div/div[1]/div/div/lego-table/div/div[3]/div[2]/div[2]").text
+    "/html/body/app-bootstrap/ng2-bootstrap/bootstrap/div/div/div/div/div/div[1]/div[2]/div/div[1]/div/div[1]/div/lego-report/lego-canvas-container/div/file-drop-zone/span/content-section/div[3]/canvas-component/div/div/div[1]/div/div/lego-table/div/div[3]/div[2]/div[2]").text
 
 with open("tmp.csv", mode="w") as tmpCSV:
     wrt = csv.writer(tmpCSV)
