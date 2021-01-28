ed46d6372dc09268e1251705dca28449c0f2efbb
computergeek1100 <66804731+computergeek1100@users.noreply.github.com>
2021-01-27 21:13:17 -0500

    Update vaxGraph.R

--- scripts/vaxGraph.R
+++ scripts/vaxGraph.R
@@ -25,22 +25,21 @@ if(all(vaxVector==as.character(tail(vaxData,1)))){ # Check if data in graph
   vaxDataCleaned$date <- as.Date(vaxDataCleaned$date, format="%b %d, %Y")
   vaxDataCleaned$totalDose1 <- as.numeric(gsub(",","",vaxDataCleaned$totalDose1))
   vaxDataCleaned$totalDose2 <- as.numeric(gsub(",","",vaxDataCleaned$totalDose2))
-  vaxDataCleaned$totalDose1 <- vaxDataCleaned$totalDose1 - vaxDataCleaned$totalDose2
   vaxDataCleaned <- vaxDataCleaned %>%
-    mutate(totalDoses = totalDose1+totalDose2,
-           dose1PriorDay = totalDose1 - lag(totalDose1),
-           dose2PriorDay = totalDose2 - lag(totalDose2),
-           totalDosesPriorDay = totalDoses - lag(totalDoses))
+    mutate(dose1Only = totalDose1 - totalDose2,
+           totalDoses = totalDose1+totalDose2,
+           totalDosesPriorDay = totalDoses - lag(totalDoses),)
   vaxData_GRAPH <- vaxDataCleaned%>%
-    select(1:3)%>%
+    select(1,4,3)%>%
     pivot_longer(c(2:3), names_to = "dose", values_to = "number")
   vaxData_GRAPH <- vaxData_GRAPH[-c(1:2),]
   vaxGraph <- ggplot(vaxData_GRAPH, aes(date, number, fill=as.factor(dose), text = paste0("Date: ", date,
                                                                                           "\n", c("First Dose", "Fully Vaccinated"), ": ", formatC(number, format = "d", big.mark = ","))))+
-    geom_col(data=subset(vaxData_GRAPH, dose=="totalDose1" | dose=="totalDose2"), position=position_stack(reverse=F))+
+    geom_col(position=position_stack(reverse=F))+
     labs(title=paste0("Last Updated: ", format(tail(vaxDataCleaned$date, 1), "%b %d, %Y"),
-                      "<sup>\nFirst Dose Only: ", formatC((tail(vaxDataCleaned$totalDose1, 1)), format = "d", big.mark = ","),
-                      "\t\t\tFully Vaccinated: ", formatC((tail(vaxDataCleaned$totalDose2, 1)), format = "d", big.mark = ",")),
+                      "<sup>\nFirst Dose Only: ", formatC((tail(vaxDataCleaned$dose1Only, 1)), format = "d", big.mark = ","),
+                      "\t\t\tFully Vaccinated: ", formatC((tail(vaxDataCleaned$totalDose2, 1)), format = "d", big.mark = ","),
+                      "\t\t\tDoses Administered Since Last Update: ", formatC(tail(vaxDataCleaned$totalDosesPriorDay, 1), format = "d", big.mark = ",")),
          margin = 30, x = "Date", y = "People Vaccinated")+
     scale_fill_brewer(name="Dose", palette="Set1")
 
@@ -48,10 +47,10 @@ if(all(vaxVector==as.character(tail(vaxData,1)))){ # Check if data in graph
     config(displayModeBar=FALSE)%>%
     layout(yaxis=list(rangemode="tozero"))
   
-  vaxGraph$x$data[[1]]$name <- "First Dose"
+  vaxGraph$x$data[[1]]$name <- "First Dose Only"
   vaxGraph$x$data[[2]]$name <- "Fully Vaccinated"
   
-  # vaxGraph # preview graph - comment out if unnecessary
+  vaxGraph # preview graph - comment out if unnecessary
 
   htmlwidgets::saveWidget(vaxGraph, file="../graphs/vaccinations.html",selfcontained=FALSE,libdir="../graphs/plotlyJS",title='vaccinations')
 }
