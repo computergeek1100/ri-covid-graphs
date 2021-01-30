497d2288f2ae6cbd45b386a1598755061509fb95
computergeek1100 <mjeise1@gmail.com>
2021-01-29 20:29:49 -0500

    Removed log file

---
+++ scripts/UPDATE.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd
+
+cd ri-covid-graphs/scripts
+
+echo > sh-log.txt
+
+git pull ri-covid-graphs gh-pages
+
+Rscript dailyGraphs.R
+
+Rscript vaxGraph.R
+
+git commit -a -m "Daily graph update"
+
+git push
