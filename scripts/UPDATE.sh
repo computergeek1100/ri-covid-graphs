207c763bcac7d1fd4533fae307a8ab7f42d82738
computergeek1100 <mjeise1@gmail.com>
2021-02-02 13:14:03 -0500

    Daily graph update

--- scripts/UPDATE.sh
+++ scripts/UPDATE.sh
@@ -14,7 +14,7 @@ Rscript vaxGraph.R
 
 cd ../graphs/plotlyJS/plotly-htmlwidgets-css-1.57.1
 
-if ! [cmp -s plotly-css-BACKUP.css plotly-htmlwidgets.css]
+if [diff plotly-css-BACKUP.css plotly-htmlwidgets.css]
 then
 cp plotly-css-BACKUP.css plotly-htmlwidgets.css
 fi
