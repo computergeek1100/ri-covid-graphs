7d199a6065174728dd14143cf5bddad66ede1924
computergeek1100 <mjeise1@gmail.com>
2021-02-02 13:07:38 -0500

    Daily graph update

--- scripts/UPDATE.sh
+++ scripts/UPDATE.sh
@@ -14,7 +14,10 @@ Rscript vaxGraph.R
 
 cd ../graphs/plotlyJS/plotly-htmlwidgets-css-1.57.1
 
+if [! cmp -s plotly-css-BACKUP.css plotly-htmlwidgets.css]
+then
 cp plotly-css-BACKUP.css plotly-htmlwidgets.css
+fi
 
 cd ../../../
 git commit -a -m "Daily graph update"
