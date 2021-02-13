102789ae8031c887967f17d2438c3b1873471268
computergeek1100 <66804731+computergeek1100@users.noreply.github.com>
2021-02-13 11:57:30 -0500

    Update dailyGraphs.R

--- scripts/dailyGraphs.R
+++ scripts/dailyGraphs.R
@@ -4,32 +4,7 @@ library(zoo)
 library(htmlwidgets)
 library(googlesheets4)
 
-# Custom functions
-
-ggArgs <- function(gg, lab1 = NULL, lab2 = NULL){
-  plt <- ggplotly(gg, tooltip="text",dynamicTicks=TRUE, originalData=FALSE)%>%
-    config(displayModeBar=FALSE)%>%
-    layout(yaxis=list(rangemode="tozero"))
-  if(!is.null(lab1) & !is.null(lab2)){
-    plt$x$data[[1]]$name <- lab1
-    plt$x$data[[2]]$name <- lab2
-  }
-  return(plt)
-}
-
-widgetArgs <- function(plt){
-  saveWidget(plt, file=paste0("../graphs/",deparse(substitute(plt)),".html"),
-             selfcontained=FALSE, libdir="../graphs/plotlyJS", title=deparse(substitute(plt)))
-}
-
-thresholdText <- function(plt, desc, linePos, textPos){
-  plt+geom_segment(x=head(stateDataCleaned$date, 1), y = linePos, xend = tail(stateDataCleaned$date, 1), yend=linePos, color="red")+
-    annotate("text", x=stateDataCleaned$date[round(nrow(stateDataCleaned)/2, 0)],y=textPos, label=desc, color = 'red', size = 5)
-}
-
-numFormat <- function(num){
-  return(formatC((num), format = "d", big.mark = ","))
-}
+source("FUNCTIONS.R")
 
 # Read data
 
