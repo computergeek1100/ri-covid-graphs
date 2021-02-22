44731e778b3eaf5a73a41f338b52c87efb301180
computergeek1100 <66804731+computergeek1100@users.noreply.github.com>
2021-02-21 21:09:51 -0500

    Changed CDC graph to color-blind friendly palette

--- scripts/update_cdc.R
+++ scripts/update_cdc.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(plotly)
 
+cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
 CDC_NE <- read.csv("https://raw.githubusercontent.com/youyanggu/covid19-cdc-vaccination-data/main/aggregated_adjusted.csv")%>%
   filter(Location == "RI" | Location == "CT" | Location == "MA" | Location == "NH" | Location == "VT" | Location == "ME")%>%
   select(date = Date, State = Location, dist = Doses_Distributed, admin = Doses_Administered)%>%
@@ -21,8 +23,11 @@ if(identical(CDC_NE, CDC_NE_prev)){
     geom_line()+
     geom_line(data=CDC_NE[!is.na(CDC_NE$pct), ], linetype="dashed")+
     labs(title=paste0("Percent of Doses Administered (New England)\n<sup>Data from CDC.gov (", as.character(tail(CDC_NE$date, 1), format="%b %d, %Y"), ") | Dashed lines represent days with no data reported</sup>"),
-         x = "Date", y = "% Doses Administered")
+         x = "Date", y = "% Doses Administered")+
+    scale_color_manual(values=cbPalette)
   vaccine_percent_NE <- ggArgs(vaccine_percent_NE)
+  
+  vaccine_percent_NE
   
   saveRDS(vaccine_percent_NE, "../graphs/vaccine_percent_NE.rds")
   
